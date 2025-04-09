10515e724e8a22d558b7483e32760b7c70b1815f
silvia <silviadesiderii01@gmail.com>
2025-04-09 15:45:50 +0200

    esercizio svolto

--- React/esercizi-react/Components/AlertClock.jsx
+++ React/esercizi-react/Components/AlertClock.jsx
@@ -1,13 +1,10 @@
-function NewDate() {
-  const date = new Date().toLocaleTimeString();
-  alert(`The current time is: ${date}`);
-}
-
-export function AlertClock() {
+export function AlertClock(props) {
   return (
     <div>
-      <p><em>Click the button to show current time!</em></p>
-      <button className="button" onClick={NewDate}>
+      <p>
+        <em>Click the button to show current time!</em>
+      </p>
+      <button className="button" onClick={props.date}>
         {" "}
         What time is it?
       </button>
--- React/esercizi-react/src/App.jsx
+++ React/esercizi-react/src/App.jsx
@@ -3,6 +3,11 @@ import { Welcome } from "../Components/Welcome"; //importo il componente Welcome
 import { AlertClock } from "../Components/AlertClock";
 import "./App.css";
 
+//Qui dichiaro la funzione che gestirà l'evento e verrà assegnata al valore di onClick come props
+function NewDate() {
+  const date = new Date().toLocaleTimeString();
+  alert(`The current time is: ${date}`);
+}
 function App() {
   return (
     <>
@@ -10,7 +15,8 @@ function App() {
       {/* Chiamata del componente/rendering*/}
       {/* <Welcome name="Silvia" age={24} /> */}
       {/*valorizzo la props name ed age, assegnando il valore all'attributo.*/}
-      <AlertClock />
+      <AlertClock date={NewDate} />{" "}
+      {/* passo la funzione che gestisce l'evento come props del componente AlertClock */}
     </>
   );
 }
