e798e39a98e6975064853d3930ab370762162ec3
silvia <silviadesiderii01@gmail.com>
2025-06-18 20:13:50 +0200

    svolto esercizio controllers

---
+++ Esercizi Node.js-API/src/Controllers/Planets.ts
@@ -0,0 +1,110 @@
+import { Request, Response } from "express";
+
+//alias di tipo Planets con corrispettivi tipi da rispettare nell'utilizzo.
+type Planet = {
+  id: string;
+  name: string;
+};
+
+type Planets = Planet[]; //la variabile tipizzata a planets deve essere un Array che rispetti la tipizzazione di Planet. (id:number, name:string)
+
+let planets: Planets = [
+  {
+    id: "1",
+    name: "Earth",
+  },
+  {
+    id: "2",
+    name: "Mars",
+  },
+];
+//GET
+const getAllPlanets = (req: Request, res: Response) => {
+  res.status(200).json(planets); //passiamo il json creato nella risposta dell'API
+  //.json definisci tu che il content/type sarà application/json.
+};
+
+//GET
+const getOnePlanetById = (req: Request, res: Response) => {
+  //:id è un parametro dinamico che cattura il valore numerico nell'URL.
+  const { id } = req.params; // Estrae il valore del parametro id dall'URL.
+ //Cerca nell'array planets il pianeta il cui id corrisponde al id estratto dall'URL.
+  const planetID = planets.find((planet) => planet.id === id);
+
+  // Gestione dell'errore: se il pianeta non viene trovato
+  // Se planetID è undefined (cioè find non ha trovato nulla), inviamo un errore 500
+  if (!planetID) {
+    console.log(`Errore: Pianeta con ID ${id} non trovato.`);
+
+    // Invia una risposta 500 di errore al client
+    return res
+      .status(500)
+      .send({ message: `Errore: Pianeta con ID ${id} non trovato.` });
+  }
+
+  // Se il pianeta viene trovato, inviamo una risposta 200 con i dati del pianeta
+  res.status(200).json(planetID);
+};
+
+//POST
+const createNewPlanet = (req: Request, res: Response) => {
+  //REQ: oggetto con tutte le informazioni sulla richiesta HTTP in arrivo, inclusi i dati inviati dal client (ad esempio, il corpo della richiesta), gli header, i parametri dell'URL, ecc.
+   //RES: oggetto che viene utilizzato per costruire e inviare la risposta HTTP al client.
+  const { id, name } = req.body;
+  const newPlanet: Planet = { id, name };
+  planets = [...planets, newPlanet];
+
+  res.status(201).json({
+    msg: `Nuovo pianeta creato: id:${newPlanet.id}, name:${newPlanet.name} `,
+  });
+  console.log(newPlanet, planets);
+};
+
+
+//PUT
+const updatePlanetById = (req: Request, res: Response) => {
+  const { id } = req.params; // Estrazione dell'ID dal parametro URL
+  const { name } = req.body; // Estrazione del nuovo nome dal corpo della richiesta
+
+  // Variabile per tracciare se un pianeta è stato trovato e aggiornato
+  let found = false;
+
+  //Aggiornamento dell'array 'planets'
+  planets = planets.map((planet) => {
+    if (planet.id === id) {
+      // Se l'ID del pianeta corrente corrisponde all'ID richiesto
+      found = true; // Imposta 'found' a true
+      return { ...planet, name }; //  E Restituisce un nuovo oggetto pianeta con il nome aggiornato
+    }
+    return planet; // Se l'ID non corrisponde, restituisce il pianeta originale senza modifiche
+  });
+
+  // Gestione del caso in cui il pianeta non venga trovato
+  if (!found) {
+    return res.status(404).json({ msg: `Pianeta con ID ${id} non trovato.` });
+  }
+  console.log(planets);
+
+  res.status(200).json({
+    msg: `Il pianeta con ID ${id} è stato aggiornato con il nome: ${name}`,
+  });
+};
+
+
+//DELETE
+const deletePlanetById = (req: Request, res: Response) => {
+  const { id } = req.params;
+  planets = planets.filter((p) => p.id !== id);
+
+  res.status(200).json({ msg: `il pianeta con id ${id} è stato eliminato. ` });
+
+  console.log(planets);
+};
+
+export {
+  getAllPlanets,
+  getOnePlanetById,
+  createNewPlanet,
+  updatePlanetById,
+  deletePlanetById,
+};
--- Esercizi Node.js-API/src/serverPlanets.ts
+++ Esercizi Node.js-API/src/serverPlanets.ts
@@ -3,6 +3,13 @@ import "express-async-errors";
 //importa un modulo aggiuntivo "middleware" (che si interpone tra una richiesta e una risposta) gestisce automaticamente gli errori nelle funzioni asincrone (quelle che usano async/await) all'interno delle rotte di Express, evitando che l'applicazione si blocchi in caso di eccezioni non catturate.
 
 import morgan from "morgan"; //Morgan è un logger di richieste HTTP, ogni volta che il tuo server riceve una richiesta, logga informazioni su quella richiesta nella console (ES. il metodo HTTP, l'URL, il codice di stato della risposta, il tempo di risposta, ecc.).
+import {
+  createNewPlanet,
+  deletePlanetById,
+  getAllPlanets,
+  getOnePlanetById,
+  updatePlanetById,
+} from "./Controllers/Planets.js";
 
 const app = express();
 const port = 3000;
@@ -29,87 +36,19 @@ let planets: Planets = [
   },
 ];
 
-app.get("/api/planets", (req, res) => {
-  res.status(200).json(planets); //passiamo il json creato nella risposta dell'API
-  //.send metodo che: imposta automaticamente l'header Content-Type appropriato alla richiesta.
-  //alternativa: .json (che definisci tu che il content/type sarà application/json).
-});
+app.get("/api/planets", getAllPlanets);
 
 //METODO GET : RICHIESTA.
-app.get("/api/planets/:id", (req, res) => {
-  //:id è un parametro dinamico che cattura il valore numerico nell'URL.
-  const { id } = req.params; // Estrae il valore del parametro id dall'URL.
-
-  //Cerca nell'array planets il pianeta il cui id corrisponde al id estratto dall'URL.
-  const planetID = planets.find((planet) => planet.id === id);
-
-  // Gestione dell'errore: se il pianeta non viene trovato
-  // Se planetID è undefined (cioè find non ha trovato nulla), inviamo un errore 500
-  if (!planetID) {
-    console.log(`Errore: Pianeta con ID ${id} non trovato.`);
-
-    // Invia una risposta 500 di errore al client
-    return res
-      .status(500)
-      .send({ message: `Errore: Pianeta con ID ${id} non trovato.` });
-  }
-
-  // Se il pianeta viene trovato, inviamo una risposta 200 con i dati del pianeta
-  res.status(200).json(planetID);
-});
+app.get("/api/planets/:id", getOnePlanetById);
 
 //METODO POST: INVIARE (AGGIUNGERE UN NUOVO PIANETA)
-app.post("/api/planets", (req, res) => {
-  //REQ: oggetto con tutte le informazioni sulla richiesta HTTP in arrivo, inclusi i dati inviati dal client (ad esempio, il corpo della richiesta), gli header, i parametri dell'URL, ecc.
-  const { id, name } = req.body; //RES: oggetto che viene utilizzato per costruire e inviare la risposta HTTP al client.
-  const newPlanet = { id, name };
-  planets = [...planets, newPlanet];
-
-  res
-    .status(201)
-    .json({
-      msg: `Nuovo pianeta creato: id:${newPlanet.id}, name:${newPlanet.name} `,
-    });
-  console.log(newPlanet, planets);
-});
-
+app.post("/api/planets", createNewPlanet);
 
 //METODO PUT: AGGIORNARE LE INFORMAZIONI
-app.put("/api/planets/:id", (req, res) => { 
-  const { id } = req.params; // Estrazione dell'ID dal parametro URL
-  const { name } = req.body; // Estrazione del nuovo nome dal corpo della richiesta
-
-  // Variabile per tracciare se un pianeta è stato trovato e aggiornato
-  let found = false;
-
-  //Aggiornamento dell'array 'planets'
-  planets = planets.map((planet) => {
-    if (planet.id === id) { // Se l'ID del pianeta corrente corrisponde all'ID richiesto
-      found = true; // Imposta 'found' a true
-      return { ...planet, name }; //  E Restituisce un nuovo oggetto pianeta con il nome aggiornato
-    }
-    return planet; // Se l'ID non corrisponde, restituisce il pianeta originale senza modifiche
-  });
-
-  // Gestione del caso in cui il pianeta non venga trovato
-  if (!found) {
-    return res.status(404).json({ msg: `Pianeta con ID ${id} non trovato.` });
-  }
-
-  console.log(planets);
-
-  res.status(200).json({ msg: `Il pianeta con ID ${id} è stato aggiornato con il nome: ${name}` });
-});
+app.put("/api/planets/:id", updatePlanetById);
 
 //METODO DELETE: eliminazione di un elemento
-app.delete("/api/planets/:id", (req, res) =>{
-  const {id} = req.params
-  planets = planets.filter(p => p.id !== (id))
-  
-  res.status(200).json({msg: `il pianeta con id ${id} è stato eliminato. `})
-
-  console.log(planets)
-})
+app.delete("/api/planets/:id", deletePlanetById);
 
 app.listen(port, () => {
   console.log(`listering on port http://localhost:${port}`);
